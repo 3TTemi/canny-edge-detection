989b02f96fbb34894d2a1247be9845ae906c1217
3TTemi <temi.btw@gmail.com>
2024-03-01 17:15:49 -0500

    hysteresis added

--- main.py
+++ main.py
@@ -54,19 +54,94 @@ def sobel_filter(blur_img):
     G = G / G.max() * 255
     theta = np.arctan2(Iy, Ix)
     return (G,theta)
+        
 
+gradient, theta = sobel_filter(blurred_img)
 
+def no_max_supression(G, theta):
 
+    row, col = G.shape
+    Z = np.zeros((row,col), np.int32)
 
-print(sobel_filter(blurred_img))
+    angle = theta * 180. / np.pi 
+    angle[angle < 0] += 180 
+
+    for i in range (1, row - 1):
+        for j in range (1, col - 1):
+            q = 255 
+            r = 255 
+
+            if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+                r = [i, j-1]
+                q = G[i, j+1]
+
+            elif (22.5 <= angle[i,j] < 67.5):
+                r = G[i-1, j+1]
+                q = G[i+1, j-1]
+
+            elif (67.5 <= angle[i,j] < 112.5):
+                r = G[i-1, j]
+                q = G[i+1, j]
+
+            elif (112.5 <= angle[i,j] < 157.5):
+                r = G[i+1, j+1]
+                q = G[i-1, j-1]
+
+            if ((G[i,j] >= q) and (G[i,j] >= r)).all():
+                Z[i,j] = G[i,j]
+            else:
+                Z[i,j] = 0
+
+    return Z 
+
+suppressed_img = no_max_supression(gradient, theta)
 
 # axs[0].imshow(grey_img, cmap=plt.get_cmap('gray'))
-# axs[1].imshow(blurred_img, cmap=plt.get_cmap('gray'))
 
 
+def double_threshold(img, lowRatio, highRatio):
+
+    highThreshold = img.max() * highRatio
+    lowThreshold = lowRatio * highRatio
+
+    width, height = img.shape 
+    result = np.zeros((width,height), np.int32)
+
+    weak_pixel_val = 30 
+    strong_pixel_val = 255 
+
+    strong_i, strong_j = np.where(img >= highThreshold)
+    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
+
+    result[strong_i,strong_j] = strong_pixel_val
+    result[weak_i,weak_j] = weak_pixel_val
+
+    return result 
+
+threshold_img = double_threshold(suppressed_img, 0.07, 0.1)
+
+axs[0].imshow(threshold_img, cmap=plt.get_cmap('gray'))
 
 
-# np.convolve(img, )
-    
+def hysteresis(img, weak, strong):
+    for i in range(1,img.shape[0] - 1):
+        for j in range(1,img.shape[1] - 1):
+             if (img[i, j] == weak):
+                if (
+                    (img[i+1, j-1] == strong) or (img[i+1, j] == strong) or
+                    (img[i+1, j+1] == strong) or (img[i, j-1] == strong) or
+                    (img[i, j+1] == strong) or (img[i-1, j-1] == strong) or
+                    (img[i-1, j] == strong) or (img[i-1, j+1] == strong)
+                ):
+                    img[i, j] = strong
+                else:
+                    img[i, j] = 0
 
-# plt.show()
+    return img
+
+final_img = hysteresis(threshold_img, 30, 255)
+
+axs[1].imshow(final_img, cmap=plt.get_cmap('gray'))
+
+
+plt.show()
